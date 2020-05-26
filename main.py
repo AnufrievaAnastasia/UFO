4388528857849be4a365c41924f2a42bba3c08b2
AnufrievaAnastasia <anufrieva_01@mail.ru>
2020-05-27 00:01:18 +0700

    1

---
+++ main.py
@@ -0,0 +1,23 @@
+from solution import *
+import turtle as tr
+import time
+
+start_time = time.time()
+timer = int(input('Введите время работы программы: '))
+tr.tracer(0)
+tr.hideturtle()
+ufo1 = Ufo('Пришелец-1', 50, 50, 50, 'green', 'black', 'yellow', 'blue', 5, 6, 5)
+ufo2 = Ufo('Пришелец-2', -50, 50, 20, 'red', 'pink', 'blue', 'yellow', 3, 4, 10)
+
+ufo1.show()
+ufo2.show()
+
+while (time.time() - start_time) < timer:
+    tr.clear()
+    ufo1.move()
+    ufo2.move()
+
+tr.done()
+
+print(ufo1, '\n')
+print(ufo2.engine_grade)
